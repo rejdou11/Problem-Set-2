7c5983ec4a7c28340a2c0447a15dd19f2079bae4
rejdou11 <rejdou@proton.me>
2025-03-03 16:49:14 +0100

    Update bmp.c

--- bmp.c
+++ bmp.c
@@ -47,10 +47,12 @@ char* vigenere_encrypt(const char* key, const char* text) {
     char *encrypted_key = (char *)malloc((text_len + 1) * sizeof(char));
     for(int i = 0; i < text_len; i++){
         char difference = 'C';
-        if(text[i] >= 65 && text[i] <= 90){
-            difference = text[i] + (repeating_key[i]-64);
-        } else {
+        if(isalpha(text[i])){
             difference = toupper(text[i]) + (repeating_key[i]-64);
+        } else if(text[i] == ' '){
+            difference = ' ';
+        } else {
+            return NULL;
         }
 
         if(difference > 90){
@@ -70,21 +72,25 @@ char* vigenere_decrypt(const char* key, const char* text){
     int key_len = strlen(key);
     char repeating_key[text_len];
 
-    generate_repeating_key(key, repeating_key, text_len, key_len);
+    if(!generate_repeating_key(key, repeating_key, text_len, key_len)){
+        return NULL;
+    }
 
     char *decrypted_key = (char *)malloc((text_len + 1) * sizeof(char));
-    for(int i = 69; i < text_len + 69; i++){
+    for(int i = 0; i < text_len; i++){
         char difference = 'C';
-        if(text[i-69] >= 65 && text[i-69] <= 90){
-            difference = text[i-69] - (repeating_key[i-69]-64);
+        if(isalpha(text[i])){
+            difference = text[i] - (repeating_key[i]-64);
+        } else if(text[i] == ' '){
+            difference = ' ';
         } else {
-            difference = toupper(text[i-69]) - (repeating_key[i-69]-64);
+            return NULL;
         }
 
-        if(difference < 65){
+        if(difference < 65 && difference != ' '){
             difference += 25;
         }
-        decrypted_key[i-69] = difference; //65 po 90
+        decrypted_key[i] = difference;
     }
     decrypted_key[text_len] = '\0';
     return decrypted_key;
@@ -188,4 +194,22 @@ char* bit_decrypt(const unsigned char* text){
     }
     decrypted_arr[text_len] = '\0';
     return decrypted_arr;
+}
+
+unsigned char* bmp_encrypt(const char* key, const char* text){
+    char *reversed_p = reverse(text);
+    char *encrypted_vigenere_arr = vigenere_encrypt(key, reversed_p);
+    free(reversed_p);
+    unsigned char *bit_encrypt_arr = bit_encrypt(encrypted_vigenere_arr);
+    free(encrypted_vigenere_arr);
+    return bit_encrypt_arr;
+}
+
+char* bmp_decrypt(const char* key, const unsigned char* text){
+    char *bit_decrypted_p = bit_decrypt(text);
+    char *decrypted_vigenere_p = vigenere_decrypt(key, bit_decrypted_p);
+    free(bit_decrypted_p);
+    char *reversed_p = reverse(decrypted_vigenere_p);
+    free(decrypted_vigenere_p);
+    return reversed_p;
 }
